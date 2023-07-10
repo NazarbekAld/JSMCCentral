170b2b3a22eb9883e6273b4ea0b384908eab173d
NazarXeXe <86530895+NazarbekAld@users.noreply.github.com>
2023-07-10 13:51:53 -0700

    Create example.js

---
+++ sample/example.js
@@ -0,0 +1,31 @@
+import * as Std from "../libs/std.mjs";
+
+console.info("Hi");
+
+Std.Scheduler.task(() => {
+
+    // Sync task that executes after start of the script.
+    
+    Std.Listener.on(Loader, "PlayerJumpEvent", (event) => 
+    { event.getPlayer().sendMessage("You jumped.") })
+    
+    Std.Listener.on(Loader, "ScriptDisableEvent", (event) => {console.info("BYE")});
+
+    class OwnCommand extends Std.Command {
+
+        constructor() {
+            super("owncommand")
+        }
+
+        execute(sender, args) {
+            sender.sendMessage("Yo")
+        }
+        tab(sender, args) {
+            sender.sendMessage("Wassup")
+        }
+
+    }
+
+    let owncommand = new OwnCommand();
+    owncommand.register();
+});
